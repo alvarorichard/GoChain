123a72b33d5b7c5deb942897fd7515014ea0e3fd
alvarorichard <alvaro10048@protonmail.com>
2023-06-29 14:59:41 -0300

    add dependecies and packages

---
+++ go.mod
@@ -0,0 +1,9 @@
+module example.com/GoChain
+
+go 1.19
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
 import (
-	"crypto"
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 )
+
+type Block struct {
+	Index     int
+	Timestamp string
+	BPM       int
+	Hash      string
+	PrevHash  string
+
+}
+
