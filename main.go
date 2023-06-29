238bc0a0dd2b725c1c003a5e28e7b9983a81d1a6
alvarorichard <alvaro10048@protonmail.com>
2023-06-29 15:31:52 -0300

    Block Validation

--- main.go
+++ main.go
@@ -51,4 +51,22 @@ func generateBlock(oldBlock Block, BPM int) (Block, error){
 
 }
 
+func isBlockValid(newBlock, oldBlock Block) bool {
+
+	if oldBlock.Index + 1 != newBlock.Index {
+		return false
+	}
+
+	if oldBlock.Hash != newBlock.PrevHash {
+		return false
+	}
+
+	if calculateHash(newBlock) != newBlock.Hash {
+		return false
+	}
+
+	return true
+
+}
+
 
