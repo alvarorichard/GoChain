bbd14fe7c4e04311a5a48a5ec7e679258d49d875
alvarorichard <alvaro10048@protonmail.com>
2023-06-29 15:29:41 -0300

    function concatenates Index

--- main.go
+++ main.go
@@ -24,3 +24,31 @@ type Block struct {
 
 }
 
+var Blockchain []Block
+
+func calculateHash(block Block) string {
+
+	record := string(block.Index) + block.Timestamp + string(block.BPM) + block.PrevHash
+	h := sha256.New()
+	h.Write([]byte(record))
+	hashed := h.Sum(nil)
+	return hex.EncodeToString(hashed)
+}
+
+func generateBlock(oldBlock Block, BPM int) (Block, error){ 
+
+	var newBlock Block
+
+	t := time.Now()
+
+	newBlock.Index = oldBlock.Index + 1
+	newBlock.Timestamp = t.String()
+	newBlock.BPM = BPM
+	newBlock.PrevHash = oldBlock.Hash
+	newBlock.Hash = calculateHash(newBlock)
+
+	return newBlock, nil
+
+}
+
+
