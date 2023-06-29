778911cf7d00fc1a7758dd17c73424fcf20b6032
alvarorichard <alvaro10048@protonmail.com>
2023-06-29 16:01:53 -0300

    finish

--- main.go
+++ main.go
@@ -127,6 +127,70 @@ type Message struct {
 
 }
 
+func handleWriteBlock(w http.ResponseWriter, r *http.Request) {
+	var m Message
+
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&m); err != nil {
+
+		respondWithJSON(w, r, http.StatusBadRequest, r.Body)
+		return
+
+	}
+	 defer r.Body.Close()
+
+	 newBlock, err := generateBlock(Blockchain[len(Blockchain)-1], m.BPM)
+
+	 if err != nil {
+		 respondWithJSON(w, r, http.StatusInternalServerError, m)
+		 return
+	 }
+
+	 if isBlockValid(newBlock, Blockchain[len(Blockchain)-1]) {
+		 newBlockchain := append(Blockchain, newBlock)
+		 ReplaceChain(newBlockchain)
+		 spew.Dump(Blockchain)
+		
+	 }
+
+	 respondWithJSON(w, r, http.StatusCreated, newBlock)
+
+}
+
+func respondWithJSON(w http.ResponseWriter, r *http.Request, code int, payload interface{}) {
+	
+	response, err := json.MarshalIndent(payload, "", " ")
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("HTTP 500: Internal Server Error"))
+		return
+	}
+
+	w.WriteHeader(code)
+	w.Write(response)
+
+}
+
+func main() {
+	
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go func() {
+		t := time.Now()
+		genesisBlock := Block{0, t.String(), 0, "", ""}
+		spew.Dump(genesisBlock)
+		Blockchain = append(Blockchain, genesisBlock)
+
+	}()
+
+	log.Fatal(run())
+
+}
+
 
 
 
